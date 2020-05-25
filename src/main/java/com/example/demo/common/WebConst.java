094e9b5251c4f76e3275f290f11cc80f167f1e61
riki <riki@rikinoMacBook-Air.local>
2020-05-26 08:58:26 +0900

    searchCovidCountry

--- src/main/java/com/example/demo/common/WebConst.java
+++ src/main/java/com/example/demo/common/WebConst.java
@@ -4,8 +4,8 @@ package com.example.demo.common;
  * Constant definition
  */
 public interface WebConst {
-
-    /** ---- URLs ---- **/
+	
+    /** ---- Function URLs ---- **/
 
 	String LOGIN_AUTHENTICATION_URL = "/loginAuthentication";
 
@@ -16,6 +16,8 @@ public interface WebConst {
     String SIGNUP_REGISTER_URL = "/signupRegister";
 
     String COVID_SEARCH_SUMMARY_URL = "/covidSearchSummary";
+
+    String COVID_SEARCH_COUNTRY_URL = "/covidSearchCountry";
     
     /** ---- api.covid19api.com ---- **/
 
@@ -23,5 +25,26 @@ public interface WebConst {
     
     String COVID19_API_SUMMARY_URL = COVID19_API_COM_URL + "/summary";
     
+    String COVID19_API_COUNTRY_URL = COVID19_API_COM_URL + "/country/";
+    
+	/** ---- Date Words ---- **/
 
+	String FROM = "from";
+
+	String TO = "to";
+
+	int COVID19_START_YEAR = 2020;
+
+	int COVID19_START_MONTH = 1;
+
+	int COVID19_START_DAY = 1;
+	
+	/** ---- URL Parameter Words ---- **/
+
+	String EQUAL_MARK = "=";
+
+	String QUESION_MARK = "?";
+
+	String AND_MARK = "&";
+    
 }
