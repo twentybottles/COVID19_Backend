7e8883c936cf97c4451c28fbb86b3e887fc365c3
riki <riki@rikinoMacBook-Air.local>
2020-06-26 11:10:59 +0900

    jenkins test

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -22,7 +22,7 @@ public class LoginController {
 	
 		CsrfToken token = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
 		
-		String aaa = "ccc";
+		String aaa = "ddd";
 	    
 		return "".equals(token.getToken()) ? false : true;
 		
